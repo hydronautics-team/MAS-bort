97977c8fd377b1fc47c850f53932a083c3fb9acc
Aleksey Novikov <nan17m521@yandex.ru>
2023-09-21 04:31:13 +0300

    ROS2 UDP communication node: add send opearation

--- ros2/udp_publisher/src/publisher.cpp
+++ ros2/udp_publisher/src/publisher.cpp
@@ -8,38 +8,56 @@
 
 #include "udp_client.h"
 
+
 using namespace std::chrono_literals;
 
-class MinimalPublisher : public rclcpp::Node
-{
+class MinimalPublisher : public rclcpp::Node {
 public:
-  MinimalPublisher()
-  : Node("minimal_publisher")
-  {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("udp_message", 1);
-    udp_client_ = std::make_shared<UdpClient::UdpClient>(
-      [&](std::string recv_msg){ this->udp_recv_callback(recv_msg); },
-      this->get_logger()
-    );
-    std::thread([&](){ udp_client_->receisver_task(); }).detach();
-  } 
+    static constexpr int TIMER_RATE_MSEC = 100;
 
-  void udp_recv_callback(std::string recv_msg) {
-    auto message = std_msgs::msg::String();
-    message.data = recv_msg;
-    publisher_->publish(message);
-  }
+    MinimalPublisher()
+    : Node("minimal_publisher")
+    , publisher_(this->create_publisher<std_msgs::msg::String>("udp_message", 1))
+    , subscription_(this->create_subscription<std_msgs::msg::String>("planner_message", 1,
+        std::bind(&MinimalPublisher::planner_msg_callback, this, std::placeholders::_1)))
+    , udp_client_(std::bind(&MinimalPublisher::recv_callback, this, std::placeholders::_1),
+        this->get_logger()) {
+        std::thread([&](){ udp_client_.run(); }).detach();
+        send_timer_ = this->create_wall_timer(std::chrono::milliseconds(TIMER_RATE_MSEC),
+            std::bind(&MinimalPublisher::timer_callback, this));
+    }
 
 private:
-  std::shared_ptr<UdpClient::UdpClient> udp_client_;
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    void planner_msg_callback(const std_msgs::msg::String &message) {
+        RCLCPP_INFO_STREAM(this->get_logger(), "Received from planner: " << message.data);
+        last_planner_message_ = message.data;
+    }
+
+    void recv_callback(std::string recv_msg) {
+        // Распарсить сообщение на разные данные и запостить в топики нужно будет где-то здесь
+
+        auto message = std_msgs::msg::String();
+        message.data = recv_msg;
+        publisher_->publish(message);
+    }
+
+    void timer_callback() {
+        // assert(!last_planner_message_.empty());
+        udp_client_.send(last_planner_message_);
+    }
+
+private:
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+    udp_client::UdpClient udp_client_;
+    rclcpp::TimerBase::SharedPtr send_timer_;
+    std::string last_planner_message_;
 };
 
 int main(int argc, char * argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<MinimalPublisher>());
+    rclcpp::shutdown();
+    return 0;
 }
--- ros2/udp_publisher/src/udp_client.h
+++ ros2/udp_publisher/src/udp_client.h
@@ -1,21 +1,18 @@
 #ifndef UDP_CLIENT
 #define UDP_CLIENT
 
-#include <boost/beast/core/flat_buffer.hpp>
 #include <thread>
 
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
 #include <rclcpp/logger.hpp>
-#include <rclcpp/logging.hpp>
-
 #include "rclcpp/rclcpp.hpp"
 
 #define IPADDRESS "127.0.0.1"
 #define UDP_PORT 5005
 
 
-namespace UdpClient {
+namespace udp_client {
 
 using boost::asio::ip::udp;
 using boost::asio::ip::address;
@@ -27,9 +24,29 @@ public:
     UdpClient(FunctionPtr callback, rclcpp::Logger logger): callback_(callback), logger_(logger) {
     }
 
+    void run() {
+        socket_.open(udp::v4());
+        socket_.bind(udp::endpoint(address::from_string(IPADDRESS), UDP_PORT));
+
+        receive_next();
+
+        RCLCPP_INFO(logger_, "Receiving");
+        io_service_.run();
+        RCLCPP_INFO(logger_, "Receiving exit");
+    }
+
+    void send(const std::string &data) {
+        if (!socket_.is_open()) {
+            RCLCPP_ERROR(logger_, "Can't send - socket is not open!");
+        }
+        socket_.async_send_to(boost::asio::buffer(data), remote_endpoint_,
+            std::bind(&UdpClient::handle_send, this, std::placeholders::_1, std::placeholders::_2));
+    }
+
+private:
     void handle_receive(const boost::system::error_code& error, size_t bytes_transferred) {
         if (error) {
-            RCLCPP_INFO_STREAM(logger_, "Receive failed: " << error.message());
+            RCLCPP_ERROR_STREAM(logger_, "Receive failed: " << error.message());
             return;
         }
 
@@ -46,16 +63,10 @@ public:
             std::bind(&UdpClient::handle_receive, this, std::placeholders::_1, std::placeholders::_2));
     }
 
-    void receisver_task()
-    {
-        socket_.open(udp::v4());
-        socket_.bind(udp::endpoint(address::from_string(IPADDRESS), UDP_PORT));
-
-        receive_next();
-
-        RCLCPP_INFO(logger_, "Receiving");
-        io_service_.run();
-        RCLCPP_INFO(logger_, "Receiving exit");
+    void handle_send(const boost::system::error_code& error, std::size_t /* bytes_transferred */) {
+        if(error) {
+            RCLCPP_ERROR_STREAM(logger_, "Send failed: " << error.message());
+        }
     }
 
 private:
@@ -63,6 +74,7 @@ private:
     rclcpp::Logger logger_;
     boost::asio::io_service io_service_;
     udp::socket socket_{io_service_};
+    boost::array<char, 1024> send_buffer_;
     boost::array<char, 1024> recv_buffer_;
     udp::endpoint remote_endpoint_;
 };
